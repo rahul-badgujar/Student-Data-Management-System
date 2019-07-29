00e031064079d0c5139b3fea6e67a17872bc8c92
imRB7here <rahulrb15899@gmail.com>
2019-07-29 14:53:33 +0530

    In Screen Module : added MainmenuScreen Class

--- src/Screen.py
+++ src/Screen.py
@@ -1,20 +1,36 @@
 from tkinter import *
 from tkinter import ttk
 
-class Screen(Frame):
-    def __init__(self,root):
-        self._master=root
+
+class Screen(ttk.Frame):
+    def __init__(self, root):
+        # Configure the Parent Window
+        self._master = root
         super().__init__(self._master)
 
     def draw(self):
-        self.grid(row=0,column=0)
-        self._master.rowconfigure(0,weight=1)
-        self._master.columnconfigure(0,weight=1)
+        # Draws the widgets
+        self.grid(row=0, column=0)
+        self._master.rowconfigure(0, weight=1)
+        self._master.columnconfigure(0, weight=1)
+
+
+class MainmenuScreen(Screen):
+    def __init__(self, root):
+        super().__init__(root)
+
+        self.__intro_label = ttk.Label(
+            self, text='This is gonna be our Mainmenu Screen')
+
+    def draw(self):
+        self.__intro_label.grid(row=0, column=0)
+
+        super().draw()
 
 
 if __name__ == "__main__":
-    temp_window=Tk()
+    temp_window = Tk()
     temp_window.geometry('800x600')
-    test_screen=Screen(temp_window)
+    test_screen = MainmenuScreen(temp_window)
     test_screen.draw()
     temp_window.mainloop()
