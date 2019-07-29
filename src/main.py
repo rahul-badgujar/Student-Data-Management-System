33a9a4bad1d5d0852aebf368fcd55a2e34f8ca05
imRB7here <rahulrb15899@gmail.com>
2019-07-29 14:07:27 +0530

    Project Initiated

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from System import SystemManager
+
+def main():
+    system=SystemManager('Student Data Management System','800x600')
+    system.run()
+
+if __name__=='__main__':
+    print('Running The Complete Program')
+
+    main()
