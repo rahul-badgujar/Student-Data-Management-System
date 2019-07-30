ce6574b0bf7c63059fe8eab990a6bb736e096101
imRB7here <rahulrb15899@gmail.com>
2019-07-30 15:58:23 +0530

    Screen -> AddDataScreen -> hovers added for CRS Buttons

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from System import SystemManager
 
+
 def main():
-    system=SystemManager('Student Data Management System','800x600')
+    system = SystemManager('Student Data Management System', '800x600')
     system.run()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     print('Running The Complete Program')
 
     main()
